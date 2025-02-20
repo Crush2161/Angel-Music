2b5a12dc33e42a159860be93a4c3471ff770dc10
Crush2161 <s2595229@gmail.com>
2025-02-20 19:29:52 +0530

    Update userbot.py

--- NetflixMusic/core/userbot.py
+++ NetflixMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("NetflixOfficial")
-                await self.one.join_chat("NetflixSupport")
+                await self.one.join_chat("Angel_Ki_Family")
+                await self.one.join_chat("UR_DREAM_WORLD")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("NetflixOfficial")
-                await self.one.join_chat("NetflixSupport")
+                await self.two.join_chat("Angel_Ki_Family")
+                await self.one.join_chat("UR_DREAM_WORLD")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("NetflixOfficial")
-                await self.one.join_chat("NetflixSupport")
+                await self.three.join_chat("Angel_Ki_Family")
+                await self.one.join_chat("UR_DREAM_WORLD")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("NetflixOfficial")
-                await self.one.join_chat("NetflixSupport")
+                await self.four.join_chat("Angel_Ki_Family")
+                await self.one.join_chat("UR_DREAM_WORLD")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("NetflixOfficial")
-                await self.one.join_chat("NetflixSupport")
+                await self.five.join_chat("Angel_Ki_Family")
+                await self.one.join_chat("UR_DREAM_WORLD")
             except:
                 pass
             assistants.append(5)
